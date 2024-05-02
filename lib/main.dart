98c6991fa0c120d44bb44c2ef7764398306daf26
rxzheng <richardxzheng@outlook.com>
2024-05-03 08:33:18 +1000

    created from scratch

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    'Hello World',
+  runApp(
+  );
+}
+
+//stateless is the data in the thing is immutable (doesn't change)
+//stateful is if it has data that changes (such as a counter)
+class MyApp extends StatelessWidget {
+  const MyApp({String?data}): super.(data);
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello World',
     textDirection: TextDirection.ltr,
-  ));
+    )
+  }
 }
